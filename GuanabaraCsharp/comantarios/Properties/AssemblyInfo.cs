050a1dc06f0336ef0bb9ce73c647029396bb7566
ffbermudes <108033713+ffbermudes@users.noreply.github.com>
2023-03-06 16:22:33 -0300

    Update AssemblyInfo.cs

--- GuanabaraCsharp/comantarios/Properties/AssemblyInfo.cs
+++ GuanabaraCsharp/comantarios/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("comantarios")]
+[assembly: AssemblyTitle("comentarios")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("comantarios")]
+[assembly: AssemblyProduct("comentarios")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
