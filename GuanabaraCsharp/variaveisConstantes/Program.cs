19547824dd755c9542dca3d12d28de1b1d6a6dab
ffbermudes19 <ffbermudes19@gmail.com>
2023-03-09 09:21:09 -0300

    atualizando

---
+++ GuanabaraCsharp/variaveisConstantes/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace variaveisConstantes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float numeroDecimal = 2.3f;
+            const string nome = "Filipe Freitas";
+            Console.Write(nome + " " + System.Math.PI.GetType());
+            Console.Write(numeroDecimal.GetType());
+            Console.ReadKey();
+        }
+    }
+}
--- GuanabaraCsharp/variaveisCsharp/Program.cs
+++ GuanabaraCsharp/variaveisCsharp/Program.cs
@@ -13,8 +13,8 @@ namespace variaveisCsharp
             byte n = 255; // valor máximo do tipo primitivo byte
             /*byte n2 = 256;*/ // gera erro ultrapassa a quantidade de mémoria disponível.
             short n3 = 257; // Acima de 255 tem que ser utilizado SHORT.
-            Console.WriteLine("A Variável n3 {0} ocupa" + n3.GetType());
-            Console.WriteLine("A Variável n3 {0} ocupa" + n3);
+            Console.WriteLine("A Variável n3 ocupa" + n3.GetType());
+            Console.WriteLine("A Variável n3 possui valor: " + n3);
             Console.ReadKey();
         }
     }
